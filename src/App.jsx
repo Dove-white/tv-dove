e3c56b8b9534534a5d90f94b35c698808bd34a87
Dove-White <dugbazahredeemer@gmail.com>
2023-10-30 15:27:47 +0000

    Day 1 fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./index.css";
+import NotFound from "./Components/NotFound";
+import { Routes, Route } from "react-router-dom";
+import Nav from "./Components/UI/Nav";
+import HomePage from "./Components/HomePage";
+import AboutPage from "./Components/AboutPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Shows from "./Shows";
+
+const HomePage = () => {
+  return <Shows />;
+};
+
+export default HomePage;
