d4c14deb10a7a93d3df85977b58a936ae9922512
Dove-White <dugbazahredeemer@gmail.com>
2023-10-30 15:32:28 +0000

    Starting

--- src/Components/AboutPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const AboutPage = () => {
-  return (
-    <div className="flex justify-center items-center h-[100vh]">Not yet</div>
-  );
-};
-
-export default AboutPage;
--- src/Components/HomePage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Shows from "./Shows";
-
-const HomePage = () => {
-  return <Shows />;
-};
-
-export default HomePage;
